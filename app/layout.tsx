828589106ed59be44909288eb2b467909efb58fa
shruti mehta <140491471+mehshruti@users.noreply.github.com>
2024-05-13 13:13:55 +0530

    Web To Pdf 📝

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import  Image  from "../app/pratial/images/logo.ico.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
